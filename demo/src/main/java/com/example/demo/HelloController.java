aea6e6a7592a058444d198311eabf63aa99e5372
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-06-20 16:11:02 +0200

    changes

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
+    
     @GetMapping("/api/message")
     public String getMessage() {
         return "Hello from Spring Boot!";
