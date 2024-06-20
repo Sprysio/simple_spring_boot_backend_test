70c216bc57fe03c4674b2f5c1fbd16ac8cc50fa2
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-06-20 16:46:26 +0200

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000",maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/api/message")
+    
+    @GetMapping("/message")
     public String getMessage() {
         return "Hello from Spring Boot!";
     }
